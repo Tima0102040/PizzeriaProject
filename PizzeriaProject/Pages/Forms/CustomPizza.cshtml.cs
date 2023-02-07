70f780a44ddac9cd90277df482d15ee9c381cee0
Tima0102040 <voznesenskiytim@gmail.com>
2023-02-07 22:02:04 +0200

    AddPizzaModel

--- PizzeriaProject/Pages/Forms/CustomPizza.cshtml.cs
+++ PizzeriaProject/Pages/Forms/CustomPizza.cshtml.cs
@@ -1,9 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using PizzeriaProject.Model;
 
 namespace PizzeriaProject.Pages.Forms;
 
 public class CustomPizza : PageModel
 {
+    [BindProperty]
+    public PizzasModel PizzasModel { get; set; }
     public void OnGet()
     {
         
